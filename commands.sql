a6c58b00a4e9d04d1ec08adbb99aa0ee93eefc9d
RudarDaman <damanrudar@gmail.com>
2016-04-19 01:42:36 +0530

    Update commands.sql

--- commands.sql
+++ commands.sql
@@ -2,6 +2,6 @@ create database student;
 
 use student;
 
-create table user(name varchar(100),password int);
+create table question(que varchar(100),a varchar(100),b varchar(100),c varchar(100),d varchar(100),correct varchar(100));
 
-insert into user values('Rudar',1234);
+insert into question values('Capital Of India?','Pakistan','Bangladesh','Delhi','Nepal','Delhi');
