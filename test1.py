52ad7c015e8d10c92006fc4ea1eb7df74bc571e2
Ujjwal Goyal <hg6547@gmail.com>
2016-04-20 00:40:08 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -20,13 +20,6 @@ def call():
 			print str(row)
     	return render_template("template1.html",data = data)
 
-def background_stuff():
-     print 'In background_stuff'
-     while True:
-         time.sleep(1)
-         t = str(time.clock())
-         socketio.emit('message', {'data': 'This is data', 'time': t}, namespace='/test')
-
 if __name__ == '__main__':
 	app.run(debug = True)
 
