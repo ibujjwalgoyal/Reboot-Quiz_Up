b8b56b8921484addd01a2a746af95c19b695f503
rohankanoo <rohan.kanoo@gmail.com>
2016-04-19 20:58:18 +0530

    Update commands.sql
    Added 12 Questions.
    Topic- BBC Sherlock
               Android

--- commands.sql
+++ commands.sql
@@ -5,3 +5,15 @@ use student;
 create table question(que varchar(100),a varchar(100),b varchar(100),c varchar(100),d varchar(100),correct varchar(100));
 
 insert into question values('Capital Of India?','Pakistan','Bangladesh','Delhi','Nepal','Delhi');
+insert into question values('Which Euston road in London artfully doubles for Baker Street in the show?','Malet Street','Huntley Street','Gower Street','Allington Street','Gower Street');
+insert into question values('How does Sherlock take his coffee on his first appearance?','White, one sugar','Black, two sugars','Black, no sugar','White, two sugars','Black, two sugars');
+insert into question values('What was the name of Charles Augustus Magnussen’s luxury house?','Bideford','Berryvault','Casterlyrock','Appledore','Appledore');
+insert into question values('Which Doctor Who actor auditioned for the role of Watson?','Peter Capaldi','David Tennant','Matt Smith','Arthur Darvill','Matt Smith');
+insert into question values('What is Moriarty’s ringtone?','Staying Alive','Clarity','Sympathy for the Devil','How deep is youtr love?','Staying Alive');
+insert into question values('In which of Conan Doyle’s books did Mary Morstan first appear?','The Hound of the Baskervilles','The Sign of the Four','The Valley of Fear','A Study in Scarlet','The Sign of the Four');
+insert into question values('In relation to Android, what is OTA?','Office of Technology Assessment','Operational Transconductance Amplifier','Over The Air','Output of The Application','Over The Air');
+insert into question values('The first Android Nook was released on November 30th, 2009 for how much?','$199','$259','$159','$399','$259');
+insert into question values('What Android SDK was the last to officially support Adobe System Flash Player?','Ice Cream Sandwich','Gingerbread','Honeycomb','Kitkat','Ice Cream Sandwich');
+insert into question values('What is Huawei user interface layer on top of Android called?','Oxygen','Emotion','Cynogen','Sense UI','Emotion');
+insert into question values('What system on a chip does HTC One use?','Snapdragon 430','Snapdragon 615','Snapdragon 620','Snapdragon 600','Snapdragon 600');
+insert into question values('What type of device is the Samsung Galaxy NX?','Camera','Tablet','Phablet','Smartphone','Camera');
