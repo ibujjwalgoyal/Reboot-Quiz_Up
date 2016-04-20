15d499beef25b41bef9ee4c3df0409f65088edc5
RudarDaman <damanrudar@gmail.com>
2016-04-21 01:26:40 +0530

    Update commands.sql

--- commands.sql
+++ commands.sql
@@ -7,10 +7,7 @@ create table question(que varchar(100),a varchar(100),b varchar(100),c varchar(1
 insert into question values('Capital Of India?','Pakistan','Bangladesh','Delhi','Nepal','Delhi');
 insert into question values('Which Euston road in London artfully doubles for Baker Street in the show?','Malet Street','Huntley Street','Gower Street','Allington Street','Gower Street');
 insert into question values('How does Sherlock take his coffee on his first appearance?','White, one sugar','Black, two sugars','Black, no sugar','White, two sugars','Black, two sugars');
-insert into question values('What was the name of Charles Augustus Magnussen’s luxury house?','Bideford','Berryvault','Casterlyrock','Appledore','Appledore');
 insert into question values('Which Doctor Who actor auditioned for the role of Watson?','Peter Capaldi','David Tennant','Matt Smith','Arthur Darvill','Matt Smith');
-insert into question values('What is Moriarty’s ringtone?','Staying Alive','Clarity','Sympathy for the Devil','How deep is youtr love?','Staying Alive');
-insert into question values('In which of Conan Doyle’s books did Mary Morstan first appear?','The Hound of the Baskervilles','The Sign of the Four','The Valley of Fear','A Study in Scarlet','The Sign of the Four');
 insert into question values('In relation to Android, what is OTA?','Office of Technology Assessment','Operational Transconductance Amplifier','Over The Air','Output of The Application','Over The Air');
 insert into question values('The first Android Nook was released on November 30th, 2009 for how much?','$199','$259','$159','$399','$259');
 insert into question values('What Android SDK was the last to officially support Adobe System Flash Player?','Ice Cream Sandwich','Gingerbread','Honeycomb','Kitkat','Ice Cream Sandwich');
