77ef96f1d472ef7615954a3f3bf72b94a3a2dcdf
RudarDaman <damanrudar@gmail.com>
2016-04-28 20:08:03 +0530

    Scoring

--- data.py
+++ data.py
@@ -13,22 +13,34 @@ def hello():
 	cursor.execute("Select * from question order by rand() limit 1,6")
 	data = cursor.fetchall()
 	session['questions']=data
+	session['Score']=0
 	ques_number = 1
 	print session['questions']
 	return render_template("start.html",ques = ques_number)
 
-@app.route("/ques/<int:ques_number>") 
+@app.route("/ques/<int:ques_number>",methods=['GET','POST']) 
 def call(ques_number):
+	if ques_number==1:
+		session['Score']=0
+	#print request.json['Score']
+	if request.method=='POST':
+		try:
+			session['Score']+=request.json['Score']
+		except:
+			session['Score']=request.json['Score']
+
 	if session['questions'] is None:
 		return "Empty"
 	else:
-		print ques_number
 		return render_template("template1.html",data = session['questions'],ques_number=ques_number)
 
 @app.route("/end")
 def end():
-	return render_template("end.html")       
+	try:
+		print session['Score']
+	except:
+		pass
+	return render_template("end.html",score=session['Score'])       
 
 if __name__ == '__main__':
-	app.debug = True
-	app.run(host = '192.168.0.110')
+	app.run(debug=True)
